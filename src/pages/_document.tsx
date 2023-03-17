22818d931d224e09edfb2b0c79265399a95c1909
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-03-16 22:14:02 -0400

    Create the Header component

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Main, Html, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html data-theme="winter">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
